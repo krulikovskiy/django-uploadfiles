6fc219a3ce55033d8b4419cff8458cf8c362dbf7
Nikita Krulikovskiy <it@krulikovskiy.ru>
2018-11-22 19:53:24 +0500

    fix

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import os
+
+
+def get_packages(package):
+    return [dirpath for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
 
 setup(
     name="django-uploadfiles",
@@ -8,7 +27,8 @@ setup(
     author="Krulikovskiy Nikita",
     author_email="it@krulikovskiy.com",
     url="https://github.com/krulikovskiy/django-uploadfiles",
-    packages=find_packages(exclude=['tests*']),
+    packages=get_packages('uploadfiles'),
+    package_data=get_package_data('uploadfiles'),
     include_package_data=True,
     license='BSD',
     install_requires=[
